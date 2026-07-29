e78de810cd0b245e91cea4288b3cf98a70780c36
RanceClementIDEA <rance.clement10@gmail.com>
2026-07-29 14:44:26 +0200

    OK

--- _generer_identite.py
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html lang="fr"><head><meta charset="utf-8">
-<title>CSS seul (ldlc)</title>
-
-</head><body>
-<h1>CSS seul (ldlc)</h1>
-  <div class="price">259,00 &euro;</div>
-</body></html>
